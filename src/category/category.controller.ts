b726da93a2f56ae0d13658c6d637f12388320045
karanbhatnagar29 <karanbhatnagar862@gmail.com>
2025-08-27 17:50:43 +0530

    handling authorization

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -28,7 +28,7 @@ const multer = require('multer');
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
-  @UseGuards(JwtAuthGuard, RolesGuard)
+  @UseGuards(RolesGuard)
   @Roles('admin')
   @Post('create')
   @UseInterceptors(FileInterceptor('image')) // 'image' is the field name in the multipart form
